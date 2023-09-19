062f38d6b2d89dcaab58676ac2f973f052dbe2b8
Evgeny Marinichev <evg5743@yandex.ru>
2023-09-19 12:23:58 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(TestIf.fraction(777.8));
+        System.out.println(TestIf.sumLastNums(333));
+        System.out.println(TestIf.charToNum('0'));
+        System.out.println(TestIf.isPositive(12));
+        System.out.println(TestIf.is2Digits(123));
+        System.out.println(TestIf.isUpperCase('Z'));
+        System.out.println(TestIf.isInRange(1,2,2));
+        System.out.println(TestIf.isDivisor(1,2));
+        System.out.println(TestIf.isEqual(1, 3, 4));
+    }
+}
