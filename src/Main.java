38c2c3475279edf29730b927741cdb92f247b45f
Evgeny Marinichev <evg5743@yandex.ru>
2023-09-19 12:27:23 +0300

    Rename Class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(TestIf.fraction(777.8));
-        System.out.println(TestIf.sumLastNums(333));
-        System.out.println(TestIf.charToNum('0'));
-        System.out.println(TestIf.isPositive(12));
-        System.out.println(TestIf.is2Digits(123));
-        System.out.println(TestIf.isUpperCase('Z'));
-        System.out.println(TestIf.isInRange(1,2,2));
-        System.out.println(TestIf.isDivisor(1,2));
-        System.out.println(TestIf.isEqual(1, 3, 4));
+        System.out.println(TestMethod.fraction(777.8));
+        System.out.println(TestMethod.sumLastNums(333));
+        System.out.println(TestMethod.charToNum('0'));
+        System.out.println(TestMethod.isPositive(12));
+        System.out.println(TestMethod.is2Digits(123));
+        System.out.println(TestMethod.isUpperCase('Z'));
+        System.out.println(TestMethod.isInRange(1,2,2));
+        System.out.println(TestMethod.isDivisor(1,2));
+        System.out.println(TestMethod.isEqual(1, 3, 4));
     }
 }
--- src/TestMethod.java
+++ src/TestMethod.java
@@ -1,4 +1,4 @@
-public class TestIf {
+public class TestMethod {
     public static int abs(int x) {
         if(x > 0) {
             return x;
